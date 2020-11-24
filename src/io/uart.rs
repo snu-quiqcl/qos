fe7edf064df8356ce8413929d87cb97f9656a3ad
acornzet <realacornzet@gmail.com>
2020-11-24 22:44:06 +0900

    uart pending interrupt

--- src/io/uart.rs
+++ src/io/uart.rs
@@ -1,56 +1,8 @@
 use volatile_register::{RO, RW};
-
 use crate::io::slcr;
 
-/// Initialize uart
-/// Reference: Zynq-7000 SOC TRM
-pub unsafe fn uart_init() {
-    let uart = &mut *UART_BASE;
-    if uart.cr.read() & (1<<4) != 0 { return }
-    uart.cr.write(1 << 5); // Set no parity
-    uart.cr.write(1 << 3 | 1 << 5); // Disable rx and tx
-
-    //TODO: Baudrate configuration
-    uart.cr.write(1 << 1 | 1); // Soft reset rx and tx data path
-    uart.cr.write(1 << 2 | 1 << 4); // Enable rx and tx
-}
-
-#[doc(hidden)]
-pub fn _print(args: core::fmt::Arguments) {
-    use core::fmt::Write;
-    unsafe {
-        let uart = &mut *UART_BASE;
-        uart.write_fmt(args).unwrap();
-    }
-}
-
-
-/// Wrapper for UartRegs
-pub struct Uart;
-impl Uart {
-    fn get(&self) -> &mut UartRegs {
-        unsafe {
-            &mut *(UART_BASE as *mut UartRegs)
-        }
-    }
-}
-
-pub fn write(c: u8) {
-    unsafe {
-        let uart = &mut *UART_BASE;
-        uart.write(c);
-    }
-}
-
-pub fn read() -> u8 {
-    unsafe {
-        let uart = &mut *UART_BASE;
-        uart.read()
-    }
-}
-
-
-
+const _UART_PHYS: usize = 0xe000_1000;       // Physical base address of Uart (1)   
+const _UART_VIRT: usize = 0xfff0_0000;       // Virtual base address of Uart -> UartRegs
 
 #[repr(C)]
 pub struct UartRegs {
@@ -115,10 +67,102 @@ impl core::fmt::Write for UartRegs {
     }
 }
 
+/// Wrapper for UartRegs
+pub struct Uart {
+    regs: &'static mut UartRegs,
+}
 
-const _UART_PHYS: usize = 0xe0001000;
-const UART_BASE: *mut UartRegs = 0xfff00000 as *mut UartRegs;
+impl Uart {
+    const UART_BASE: *mut UartRegs = _UART_VIRT as *mut UartRegs;
 
+    pub fn get() -> Uart {
+        Uart {
+            regs: unsafe {&mut *(Self::UART_BASE)}
+        }
+    }
+
+    pub fn setup(&self) {
+        // must be used after initializing system level control registers
+        let slcr = slcr::Slcr::get();
+        unsafe{
+            slcr.set_uart_rst(2, 2);       // reset uart 1 
+            slcr.set_uart_clk(20, 0, 2);   // 50 MHz to uart 1
+        }
+    }
+
+    pub unsafe fn config_frame(&self, chmode: u32, nbstop: u32, par: u32, chrl: u32, clk: u32) {
+        /* Character frame
+            CHMODE[9:8]   NBSTOP[7:6]   PAR[5:3]   CHRL[2:1]   CLKSEL[0]
+            CLKSEL: uart_ref_clk(0), uart_ref_clk/8(1)
+            default: chmode: normal(00), nbstop: 1 stop bit(00), par: none(100), chrl: 8 bits(00), clk: ref(00) 
+        */
+        self.regs.mr.write((chmode << 8) | (nbstop << 6) | (par << 3) | (chrl << 1) | clk);
+    }   
+
+    pub unsafe fn config_baud(&self, cd: u32, bdiv: u32) {
+        /* Baud rate (Table 19-1 in Zynq-7000 SOC TRM)
+            1. regs.cr             TX_DIS[5]   TX_EN[4]   RX_DIS[3]   RX_EN[2]   TX_RST[1]   RX_RST[0]
+            2. regs.baudgen        CD[15:0]
+            3. regs.badugen_div    BDIV[7:0]
+            default: baud rate: 921,600 -> cd: (9), bdiv: (5)           
+        */
+        self.regs.cr.write((1 << 5) | (1 << 3));    // disable TX and RX paths
+        self.regs.baudgen.write(cd);                
+        self.regs.baudgen_div.write(bdiv);
+    }
+
+    pub unsafe fn config_trig_lev(&self, trig: u32) {
+        /* RxFIFO trigger level
+            RTRIG[5:0]
+            default: trig: none(0) / when non-zero, write value between 1 - 63
+        */
+        self.regs.rxwm.write(trig);
+    }
+
+    pub unsafe fn enable_ctrl(&self) {
+        // Enable (Refer to config_baud)
+        self.regs.cr.write((1 << 1) | 1);           // reset TX and RX paths
+        self.regs.cr.write((1 << 4) | (1 << 2))     // enable TX and RX paths
+    }
+
+/*
+    pub unsafe fn config_timeout(&self) {
+        // program the receiver timeout mechanism
+    }
+
+    pub unsafe fn config_trig_irpt(&self) {
+        // RxFIFO trigger level interrupt
+    }
+
+    pub unsafe transmit(&self) {
+
+    }
+
+    pub unsafe receive(&self) {
+
+    }
+*/
+}
+
+/// Initialize uart
+/// Reference: Zynq-7000 SOC TRM
+pub unsafe fn uart_init() {
+    let uart = Uart::get();
+    if uart.regs.cr.read() & (1<<4) != 0 { return }
+    uart.config_frame(0, 0, 3, 0, 0);
+    uart.config_baud(9, 5);
+    uart.config_trig_lev(0);
+    uart.enable_ctrl();
+}
+
+#[doc(hidden)]
+pub fn _print(args: core::fmt::Arguments) {
+    use core::fmt::Write;
+    unsafe {
+        let uart = Uart::get();
+        uart.regs.write_fmt(args).unwrap();
+    }
+}
 
 #[macro_export]
 macro_rules! print {
@@ -130,3 +174,26 @@ macro_rules! println {
     () => (print!("\r\n"));
     ($($arg:tt)*) => (print!("{}\r\n", format_args!($($arg)*)));
 }
+
+
+/*
+let uart = Uart::get();
+
+
+*/
+
+/*
+pub fn write(c: u8) {
+    unsafe {
+        let uart = &mut *UART_BASE;
+        uart.write(c);
+    }
+}
+
+pub fn read() -> u8 {
+    unsafe {
+        let uart = &mut *UART_BASE;
+        uart.read()
+    }
+}
+*/
