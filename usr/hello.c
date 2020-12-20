5a11d2dcdf8d92e2a4a455d8c1a8f9cf6f00d029
hj <ijshj10@gmail.com>
2020-12-20 15:58:37 +0900

    refactor

--- usr/hello.c
+++ usr/hello.c
@@ -8,22 +8,16 @@ void _start() {
     volatile int* a1=axi(1);
     *a0=3;
     *a0=2;
-    int delay = 0x800;
+    int delay = 0x200;
     int time = delay;
     int count=0;
     if(fork()){
 	    for (int j=0; j<1000; j++){
-            for(int i=0; i<1000; i++) {
-                *a0 = (j%4) | time;
+            for(int i=0; i<4000; i++) {
+                *a0 = (j&1) | time;
                 time += delay;
-                *a0 = (j%4) | time;
-                time += delay;
-                *a0 = (j%4) | time;
-                time += delay;
-                *a0 = (j%4) | time;
-                time += delay;
-            sleep(500);
-            count++;
+                sleep(125);
+                count++;
             if(a0[1]) {
                     print("pFull");
                     yield();
@@ -37,16 +31,10 @@ void _start() {
         *a1=3;
         *a1=2;	
 	    for(int j=0; j<1000; j++){
-            for(int i=0; i<1000; i++) {
-                *a1 = (j%4) | time;
+            for(int i=0; i<4000; i++) {
+                *a1 = (j&1) | time;
                 time += delay;
-                *a1 = (j%4) | time;
-                time += delay;
-                *a1 = (j%4) | time;
-                time += delay;
-                *a1 = (j%4) | time;
-                time += delay;
-		sleep(500);
+		        sleep(125);
                 if(a1[1]) {
                     print("cFull");
                     yield();
